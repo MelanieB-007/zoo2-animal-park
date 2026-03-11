0456026ef44fa7bf684394ee32d068d85a79dc17
Melanie <git@melanie-busse.de>
2026-03-11 15:25:54 +0100

    refactoring create file for biome colors

--- pages/tiere.js
+++ pages/tiere.js
@@ -17,6 +17,7 @@ import EmptyState from "../components/page-structure/Elements/EmptyState";
 import PaginationSignpost from "../components/ui/PaginationSignpost";
 import StallLevelBadge from "../components/page-structure/Elements/StallLevelBadge";
 import GehegeBadge from "../components/page-structure/Elements/GehegeBadge";
+import { habitatColors } from "../utils/habitatConstants";
 
 const translations = {
   de: {
@@ -59,23 +60,6 @@ const translations = {
     loading: "Getting animals from the shelter...",
     noResults: "No animal found with this name...",
   },
-};
-
-const habitatColors = {
-  gras: { main: "#47610d" },
-  steppe: { main: "#924722" },
-  wald: { main: "#224c0b" },
-  berg: { main: "#39525e" },
-  savanne: { main: "#c66f12" },
-  dschungel: { main: "#4c7c07" },
-  eis: { main: "#066eb8" },
-  wasser: { main: "#4634c1" },
-  blattdickicht: { main: "#779d59" },
-  felsenwüste: { main: "#dcbc5d" },
-  süsswasser: { main: "#71fef8" },
-  salzwasser: { main: "#603bde" },
-  noctarium: { main: "#a540a2" },
-  default: { main: "#666666" },
 };
 
 export default function TiereUebersicht() {
