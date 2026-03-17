03b0ef89eb081f7aa6136d2414347b40904e7c6c
Melanie <git@melanie-busse.de>
2026-03-17 19:29:38 +0100

    insert next-i18

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+// next.config.js
+const { i18n } = require('./next-i18next.config');
+
 const nextConfig = {
   compiler: {
     // Das ist lebensnotwendig für Styled Components in Next.js!
